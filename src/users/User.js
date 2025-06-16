2ad55685450b9656b69c6f662672fd31ebe25c8c
karlo-lee <ghlee7411@gmail.com>
2025-06-16 14:56:32 +0900

    Implement user CRUD operations (#9068592de87249c4a70c564c5702a8aa)

--- src/users/User.js
+++ src/users/User.js
@@ -1,1 +1,33 @@
 // User management system (#9068592de87249c4a70c564c5702a8aa)
+
+class User {
+  constructor(id, name, email) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.createdAt = new Date();
+  }
+
+  // Get user information
+  getInfo() {
+    return {
+      id: this.id,
+      name: this.name,
+      email: this.email,
+      createdAt: this.createdAt
+    };
+  }
+
+  // Update user information
+  updateInfo(name, email) {
+    if (name) this.name = name;
+    if (email) this.email = email;
+  }
+
+  // Validate user data
+  isValid() {
+    return this.name && this.email && this.email.includes('@');
+  }
+}
+
+module.exports = User;
