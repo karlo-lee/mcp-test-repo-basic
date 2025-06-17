a9e5565370ef41a62d8fb00fcaabce46d3be0b5e
karlo-lee <ghlee7411@gmail.com>
2025-06-17 19:52:06 +0900

    Add authentication system files (#7e615673d06e485eb519acb1e9c4e3e2)

---
+++ src/auth/login.js
@@ -0,0 +1,8 @@
+// Authentication system implementation (#7e615673d06e485eb519acb1e9c4e3e2)
+
+function login(username, password) {
+  // TODO: Add real authentication logic here
+  return username === 'test' && password === 'test';
+}
+
+module.exports = { login };
