dccd3a754eaecb59c2725fdc0da941ea3af263d5
karlo-lee <ghlee7411@gmail.com>
2025-06-18 15:57:19 +0900

    Implement user CRUD operations (#1387f32024424a0e95659301808d027d)

--- src/users/UserService.js
+++ src/users/UserService.js
@@ -1,1 +1,28 @@
 // User management system (#1387f32024424a0e95659301808d027d)
+
+const User = require('./User');
+
+class UserService {
+  constructor() {
+    this.users = [];
+  }
+  create(user) {
+    this.users.push(user);
+    return user;
+  }
+  read(id) {
+    return this.users.find(u => u.id === id);
+  }
+  update(id, data) {
+    const user = this.users.find(u => u.id === id);
+    if (user) Object.assign(user, data);
+    return user;
+  }
+  delete(id) {
+    const idx = this.users.findIndex(u => u.id === id);
+    if (idx !== -1) return this.users.splice(idx, 1)[0];
+    return null;
+  }
+}
+
+module.exports = UserService;
