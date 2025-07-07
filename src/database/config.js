76735336cdb1ef3254cea3cdefc26e99198bfb30
karlo-lee <ghlee7411@gmail.com>
2025-07-07 23:31:48 +0900

    Add connection pooling files (38ef47ba3a59425180ece20dd1775da6)

---
+++ src/database/config.js
@@ -0,0 +1,9 @@
+// 38ef47ba3a59425180ece20dd1775da6
+// Database configuration for pooling
+module.exports = {
+  host: 'localhost',
+  port: 5432,
+  user: 'dbuser',
+  password: 's3cr3t',
+  database: 'mydb'
+};
