1d912445828d9a996ff7c46d7380b32c170fcbc8
karlo-lee <ghlee7411@gmail.com>
2025-07-07 12:54:25 +0900

    Add calculator module with basic functionality (d7a19e741ddc47d189ef1673d0d96a34)

---
+++ src/calculator.js
@@ -0,0 +1,23 @@
+// Calculator module implementation
+// d7a19e741ddc47d189ef1673d0d96a34
+
+class Calculator {
+  add(a, b) {
+    return a + b;
+  }
+
+  subtract(a, b) {
+    return a - b;
+  }
+
+  multiply(a, b) {
+    return a * b;
+  }
+
+  divide(a, b) {
+    if (b === 0) throw new Error('Division by zero');
+    return a / b;
+  }
+}
+
+module.exports = Calculator;
