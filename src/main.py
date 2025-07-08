d6bf0df32d892bde20097e5a6db1a58303daf96b
karlo-lee <ghlee7411@gmail.com>
2025-07-08 19:59:39 +0900

    Add main application file with required content

---
+++ src/main.py
@@ -0,0 +1,12 @@
+"""
+Basic Python application file
+"""
+
+# 9cb51b25aa86491e926c4906d2e5fb71
+
+def main():
+    """Main application function"""
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
