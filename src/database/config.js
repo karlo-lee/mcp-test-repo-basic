615459f0187869a63c2d498133273be91fc29db1
karlo-lee <ghlee7411@gmail.com>
2025-07-08 13:46:22 +0900

    Add database config with connection pooling (f7e73a67dd1b4922a8625b5d322a4c42)

---
+++ src/database/config.js
@@ -0,0 +1,10 @@
+// Database config for connection pooling (f7e73a67dd1b4922a8625b5d322a4c42)
+module.exports = {
+  host: 'localhost',
+  port: 5432,
+  user: 'dbuser',
+  password: 'dbpass',
+  database: 'mydb',
+  max: 20,
+  idleTimeoutMillis: 30000
+};
