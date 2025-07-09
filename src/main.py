10514f8d37b85ba50dc6e625d013047a03faf50e
karlo-lee <ghlee7411@gmail.com>
2025-07-09 20:18:45 +0900

    Add main application file with reference 54632fc1b1584c2eb4a1180381217acf

---
+++ src/main.py
@@ -0,0 +1,10 @@
+# Main application file
+
+# 54632fc1b1584c2eb4a1180381217acf
+
+def main():
+    """Main application function"""
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
