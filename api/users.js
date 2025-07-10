72d7b94a1002ccc1237ccb9307dd866687b103a0
karlo-lee <ghlee7411@gmail.com>
2025-07-10 10:12:03 +0900

    feat: add user management endpoints [029b2057916447b78c6c3d6889a17cc3]

--- api/users.js
+++ api/users.js
@@ -1,4 +1,32 @@
-// User API handlers
-// 54e5d91c4ba54eb7b514d9698fdac7c7
+// User management endpoint handlers
+// Tracking hash: 029b2057916447b78c6c3d6889a17cc3
 
-// Placeholder for user management API implementation
+const express = require('express');
+const router = express.Router();
+
+// GET /users - Get all users
+router.get('/', (req, res) => {
+  res.send('List of users');
+});
+
+// POST /users - Create a new user
+router.post('/', (req, res) => {
+  res.send('Create a new user');
+});
+
+// GET /users/:id - Get a single user
+router.get('/:id', (req, res) => {
+  res.send(`Get user with ID ${req.params.id}`);
+});
+
+// PUT /users/:id - Update a user
+router.put('/:id', (req, res) => {
+  res.send(`Update user with ID ${req.params.id}`);
+});
+
+// DELETE /users/:id - Delete a user
+router.delete('/:id', (req, res) => {
+  res.send(`Delete user with ID ${req.params.id}`);
+});
+
+module.exports = router;
