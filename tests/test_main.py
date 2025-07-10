72de1b5e82d1234de2661f51492fb4b4bbafab4a
karlo-lee <ghlee7411@gmail.com>
2025-07-10 09:52:44 +0900

    Initial commit for multi-files-4601435b4d0b4369b3635d618b5e3a6a

---
+++ tests/test_main.py
@@ -0,0 +1,10 @@
+# 4601435b4d0b4369b3635d618b5e3a6a
+
+import unittest
+
+class TestMain(unittest.TestCase):
+    def test_main(self):
+        self.assertEqual(main(), "Hello, World!")
+
+if __name__ == "__main__":
+    unittest.main()
