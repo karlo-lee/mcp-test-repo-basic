9090b6468cc6c28d36a38c4ca0bdf82b19c9294e
karlo-lee <ghlee7411@gmail.com>
2025-07-21 19:30:51 +0900

    Implement connection pooling b5cb93a141bd4926b9ff9495f518e98b

---
+++ src/database/connection.js
@@ -0,0 +1,14 @@
+// Database connection pooling implementation b5cb93a141bd4926b9ff9495f518e98b
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+module.exports = pool;
