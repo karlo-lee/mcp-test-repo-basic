f6bade0b3312701eb4d381989ee9879a50c56f04
karlo-lee <ghlee7411@gmail.com>
2025-08-21 20:01:15 +0900

    Add database config file a7152789c1ff49bd80646a69a368c496

---
+++ src/database/config.js
@@ -0,0 +1,7 @@
+// a7152789c1ff49bd80646a69a368c496
+module.exports = {
+  DB_HOST: process.env.DB_HOST || 'localhost',
+  DB_USER: process.env.DB_USER || 'user',
+  DB_PASSWORD: process.env.DB_PASSWORD || 'password',
+  DB_NAME: process.env.DB_NAME || 'mydb',
+};
