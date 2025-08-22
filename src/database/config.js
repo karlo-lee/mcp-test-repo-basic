6e657e31269ced0f9200d5e4ac770b65efd3fcd5
karlo-lee <ghlee7411@gmail.com>
2025-08-22 14:38:08 +0900

    Add DB config e3e4843e9e004e568c62fa7569a35213

---
+++ src/database/config.js
@@ -0,0 +1,10 @@
+// e3e4843e9e004e568c62fa7569a35213
+module.exports = {
+  user: 'dbuser',
+  host: 'localhost',
+  database: 'mydb',
+  password: 'secret',
+  port: 5432,
+  max: 20,
+  idleTimeoutMillis: 30000
+};
